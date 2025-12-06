60169f5071012886ff5ce989d9f388f8858c2a8e
CarlosFedericoZalazar <carlos-zalazar@hotmail.com>
2025-12-06 18:45:59 -0300

    correccion en index

--- js/carsApi.js
+++ js/carsApi.js
@@ -1,5 +1,6 @@
 export async function getAllCars() {
     const response = await fetch('https://api-autos-three.vercel.app/cars');
+    console.log("Respuesta de getAllCars:", response);
     if (!response.ok) throw new Error("Error al obtener autos");
     return response.json();
 }
